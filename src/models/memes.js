d8b93255bc6bf331488fda1524d82b9b428e1786
Dennis <dennis.tgt@gmail.com>
2021-06-04 01:11:43 +0700

    sdf

--- src/models/memes.js
+++ src/models/memes.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "Memes",
+      modelName: "memes",
       createdAt: "created_at",
       updatedAt: "updated_at",
     }
